56942cb3b95d1b9b413cf5c5e274339a0f6a5f75
karolis <karolis.pakalnis@simplanova.com>
2024-05-17 11:23:32 +0300

    UT for update

--- UserManagement.Data.Tests/DataContextTests.cs
+++ UserManagement.Data.Tests/DataContextTests.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Linq;
-using FluentAssertions;
 using UserManagement.Models;
 
 namespace UserManagement.Data.Tests;
@@ -42,6 +42,33 @@ public class DataContextTests
 
         // Assert: Verifies that the action of the method under test behaves as expected.
         result.Should().NotContain(s => s.Email == entity.Email);
+    }
+
+    [Fact]
+    public void Should_ChangeUsersDetails_When_EntityIsUpdated()
+    {
+        // Arrange: Initializes objects and sets the value of the data that is passed to the method under test.
+        var context = CreateContext();
+        var entity = context.GetAll<User>().First();
+
+        entity.Forename = "John";
+        entity.Surname = "Cena";
+        entity.Email = "js@gmail.com";
+        entity.IsActive = false;
+        entity.DateOfBirth = new DateTime(1980, 12, 25, 10, 30, 50, DateTimeKind.Utc);
+
+        context.Update(entity);
+
+        // Act: Invokes the method under test with the arranged parameters.
+        var result = context.GetAll<User>();
+
+        // Assert: Verifies that the action of the method under test behaves as expected.
+
+        result.First().Forename.Should().Be(entity.Forename);
+        result.First().Surname.Should().Be(entity.Surname);
+        result.First().Email.Should().Be(entity.Email);
+        result.First().IsActive.Should().Be(entity.IsActive);
+        result.First().DateOfBirth.Should().Be(entity.DateOfBirth);
     }
 
     private DataContext CreateContext() => new();
